fd892303a20c9f0b68816893d2b31d538e7da8aa
MaeveCh <victoriacheregatiamenta@gmail.com>
2025-04-20 19:23:12 +0000

    printf renomeacao

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,8 +2,8 @@
 
 int main(){
 //carta 1
-   char inicial;
-   char carta[3];
+   char opcao;
+   char carta[20];
    char nome[20];
    int populacao;
    int turismo;
@@ -11,7 +11,7 @@ int main(){
    float area;
 
    printf("Digite a inicial: \n");
-   scanf("%c", &inicial);
+   scanf("%c", &opcao);
 
    printf("Digite o código da carta: \n");
    scanf("%s", &carta);
@@ -19,22 +19,31 @@ int main(){
    printf("Digite o nome da cidade: \n");
    scanf("%s", &nome);
 
-   printf("Digite o número de habitantes: \n");
+   printf("Digite a quantidade de habitantes: \n");
    scanf("%d", &populacao);
 
    printf("Digite o número de locais turísticos: \n");
-   scanf("%d", &turismo);
+   scanf("%d\n", &turismo);
 
-   printf("Digite o Produto Interno Bruto da cidade: \n");
-   scanf("%f", &pib);
+   printf("Digite o valor Produto Interno Bruto da cidade: \n");
+   scanf("%f\n", &pib);
 
    printf("Digite a área em quilômetros: \n");
    scanf("%f", &area);
 
 
+   printf("A letra do Estado é: %c", opcao);
+   printf("O código da carta é: %s", carta);
+   printf("O nome da cidade é: %s", nome);
+   
+   printf("O número de habitantes é: %d", populacao);
+   printf("O número de pontos turísticos é: %d", turismo);
+
+   printf("O PIB da cidade é: %f", pib);
+   printf("A área da cidade é: %f", area);
 
 
-
+      return 0;
 
 
 
