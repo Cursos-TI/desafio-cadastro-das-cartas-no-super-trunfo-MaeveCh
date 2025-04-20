c22570067937fbee343fefb551bf6a655c475d47
MaeveCh <victoriacheregatiamenta@gmail.com>
2025-04-20 19:13:00 +0000

    printf teste

---
+++ super_trunfo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+//carta 1
+   char inicial;
+   char carta[20];
+   char nome[20];
+   int populacao;
+   int turismo;
+   float pib;
+   float area;
+
+   printf("Digite a inicial: \n");
+   scanf("%c", &inicial);
+
+   printf("Digite o código da carta: \n");
+   scanf("%s", &carta);
+
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", &nome);
+
+   printf("Digite o número de habitantes: \n");
+   scanf("%d", &populacao);
+
+   printf("Digite o número de locais turísticos: \n");
+   scanf("%d", &turismo);
+
+   printf("Digite o Produto Interno Bruto da cidade: \n");
+   scanf("%f", &pib);
+
+   printf("Digite a área em quilômetros: \n");
+   scanf("%f", &area);
+
+
+
+
+
+
+
+
+
+}
